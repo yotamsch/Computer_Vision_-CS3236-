2c38ce310224cd1981f5973e2c42ebe464bbf8b8
Yotam S <yotamsechayk@mail.tau.ac.il>
2018-05-30 13:54:54 +0300

    Fix vector and divide into color

--- HW3/Vector.java
+++ HW3/Vector.java
@@ -4,132 +4,131 @@ import java.util.Arrays;
 
 
 public class Vector {
-	double x;
-	double y;
-	double z;
+	private double x;
+	private double y;
+	private double z;
 
-
-	//Constructors
-	public Vector(double x, double y, double z){
+	// Constructors
+	public Vector(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
-	public Vector(String x, String y, String z){
+	public Vector(String x, String y, String z) {
 		this(Double.parseDouble(x), Double.parseDouble(y), Double.parseDouble(z));
-	}	
-
-	// Addition
-	public static Vector sum(Vector v, Vector u){
-		return new Vector(v.x + u.x, v.y + u.y, v.z + u.z);
 	}
-	public Vector add(Vector v){
-		return new Vector(this.x + v.x, this.y + v.y, this.z + v.z);
+	public Vector(Vector other) {
+		this(other.x,other.y,other.z);
 	}
 
-	public Vector Nadd(Vector v){
-		this.x+= v.x;
-		this.y+= v.y;
-		this.z+= v.z;
-		return this;
+	// Add
+	public void add(Vector other) {
+		this.x += other.x;
+		this.y += other.y;
+		this.z += other.z;
+	}
+	public void add(double num) {
+		this.x += num;
+		this.y += num;
+		this.z += num;
 	}
 
-	//Subtraction
-
-	public static Vector sub(Vector v, Vector u){
-		return new Vector(v.x - u.x, v.y - u.y, v.z - u.z);
+	// Subtract
+	public void sub(Vector other) {
+		this.x -= other.x;
+		this.y -= other.y;
+		this.z -= other.z;
+	}
+	public void sub(double num) {
+		this.x -= num;
+		this.y -= num;
+		this.z -= num;
 	}
 
-	public Vector Nsub(Vector v){
-		this.x-= v.x;
-		this.y-= v.y;
-		this.z-= v.z;
-		return this;
-	}
-
-	//Norm	
-
-	public double norm(){
-		return Math.sqrt(dotProduct(this, this));
-	}
-
-	public Vector normalize(){
-		return this.multiplyBy(1.0/this.norm());
-	}
-
-	public Vector Nnormalize(){
-		this.NmultiplyBy(1.0/this.norm());
-		return this;
-	}
-	//Multiply
-	public Vector multiplyBy(double scalar){
-		return new Vector(this.x*scalar, this.y*scalar, this.z*scalar);
-	}
-
-	public Vector NmultiplyBy(double scalar){
+	// Multiply
+	public void mul(double scalar) {
 		this.x *= scalar;
 		this.y *= scalar;
 		this.z *= scalar;
-		return this;
+	}
+	public void mul(Vector other) {
+		this.x *= other.x;
+		this.y *= other.y;
+		this.z *= other.z;
 	}
 
-	public Vector multiplyColor(Vector v){
-		return new Vector(v.x*this.x, v.y*this.y, v.z*this.z);
+	// Norm
+	public double norm() {
+		return Math.sqrt(dotProduct(this, this));
 	}
 
-	public Vector NmultiplyColor(Vector v){
-		this.x *= v.x;
-		this.y *= v.y;
-		this.z *= v.z;
-		return this;
+	// Normalize
+	public void normalize() {
+		this.mul(1.0/this.norm());
 	}
 
-	//miscellaneous
-
-	public static double dotProduct(Vector v, Vector u){
-		return (v.x*u.x + v.y*u.y + v.z*u.z);
+	// Distance
+	public double distance(Vector other) {
+		return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2) + Math.pow(this.z - other.z, 2));
 	}
 
-	public static Vector crossProduct(Vector v, Vector u){
-		return new Vector(v.y*u.z - v.z*u.y, v.z*u.x - v.x*u.z, v.x*u.y- v.y*u.x);		
+	// Trimming
+	public void trim(double min, double max) {
+		// lower bound
+		if (this.x < min) this.x = min;
+		if (this.y < min) this.y = min;
+		if (this.z < min) this.z = min;
+		// upper bound
+		if (this.x > max) this.x = max;
+		if (this.y > max) this.y = max;
+		if (this.z > max) this.z = max;
 	}
 
-	public static Vector fromPoints(Vector v, Vector u){
-		return sub(u,v);
-	}
-	public Vector projection(Vector base){
-		base = base.normalize();
-		return base.multiplyBy(dotProduct(base, this));
-	}
-
-
-	public byte[] getRGB(){
-
-		Vector v = this.multiplyBy(255);
-		byte[] result = new byte[3];
-		result[0] = (byte) (Math.round(v.x));
-		result[1] = (byte) (Math.round(v.y));
-		result[2] = (byte) (Math.round(v.z));
-		return result;
+	// Other
+	public boolean isZero() {
+		if (this.x == 0 && this.y == 0 && this.z == 0)
+			return true;
+		return false;
 	}
 
-	public String str(){
+	public String toString() {
 		return String.format("<Vector: [%.4f,%.4f,%.4f]>", x, y, z);
 	}
 
-	public double distanceFrom(Vector v){
-		//return fromPoints(this, v).norm();
-		return Math.sqrt(Math.pow(this.x-v.x,2) + Math.pow(this.y-v.y,2) + Math.pow(this.z-v.z,2)); 
-	}
-	public void fixHighs(){
-		if(this.x > 1) this.x =1;
-		if(this.y > 1) this.y =1;
-		if(this.z >1)  this.z =1;
+	// Static
+	public static double dotProduct(Vector v, Vector u) {
+		return v.x * u.x + v.y * u.y + v.z * u.z;
 	}
 
-	public boolean isZero(){
-		return (this.x==0 && this.y==0 && this.z==0);
+	public static Vector crossProduct(Vector v, Vector u) {
+		return new Vector(v.y * u.z - v.z * u.y, v.z * u.x - v.x * u.z, v.x * u.y - v.y * u.x);
+	}
+
+	public static Vector projection(Vector base, Vector other) {
+		Vector result = new Vector(base);
+		result.normalize();
+		result.mul(dotProduct(result, other));
+		return result;
 	}
 }
 
+public class Color extends Vector {
 
+	public Color(double r, double g, double b) {
+		super(r, g, b);
+	}
+	public Color(String r, String g, String b) {
+		super(r, g, b);
+	}
+	public Color(Color other) {
+		super(other);
+	}
+
+	public byte[] getRGB() {
+		byte[] result = new byte[3];
+		result[0] = (byte)(Math.round(this.x * 255));
+		result[1] = (byte)(Math.round(this.y * 255));
+		result[2] = (byte)(Math.round(this.z * 255));
+		return result;
+	}
+}
